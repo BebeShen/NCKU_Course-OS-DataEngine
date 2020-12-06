548896bd6a3008b61c2f2620bca9b40fcc9ee158
BebeShen <55395848+BebeShen@users.noreply.github.com>
2020-12-06 17:01:41 +0800

    [feature] Implement command read from file
    - Read `[command] [key] [key2]/[value]` from inputFile.
    - Add `compile` and `run` command line in README.
    
    TODO:
        [1] Insert data.
        [2] Implement command function.
        [3] Optimize the speed.

--- DataEngine.cpp
+++ DataEngine.cpp
@@ -11,18 +11,72 @@ string IntToString(int num){
     ss>>s;
     return s;
 }
-int main(){
+// .exe [inputfile]
+int main(int argc,char *argv[]){
+    // Set ENV
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
     double start_time = clock();
-    string outputfile_path = "/storage/";
-    string inputfile_path = "/storage/";
+    string tmpfile_path = "/storage";
     int number_file = 0;
-    inputfile_path.append(IntToString(number_file));
-    outputfile_path.append(IntToString(number_file));
+    string outputfile_path = "1.output";
+    string inputfile_path = "";
+    inputfile_path.append(argv[1]);
+    // outputfile_path.append(outputfile_path);
     fstream inputFile(inputfile_path,ios_base::in);
     fstream outputFile(outputfile_path,ios_base::out);
-    cout<<"input: "<<inputfile_path<<"\noutput: "<<outputfile_path<<endl;
+    cout<<"inputFile: "<<inputfile_path<<"\noutputFile: "<<outputfile_path<<endl;
+    /* Code start here */
+    // read command from inputfile
+    if(!inputFile){
+        cout<<"File doesn't exist.";
+        exit(1);
+    }
+    string instr;
+    while(!inputFile.eof()){
+        getline(inputFile,instr);
+        const char* delim = " ";
+        // extract command from instruction
+        int command_pos = instr.find_first_of(delim,0);
+        string command = instr.substr(0,command_pos);
+        instr = instr.substr(command_pos+1,instr.length());
+        cout<<command<<"|";
+        if(command == "PUT"){
+            // extract key from instruction
+            int key_pos = instr.find_first_of(delim,0);
+            long long int key = stoll(instr.substr(0,key_pos));
+            instr = instr.substr(key_pos+1,instr.length());
+            cout<<key<<"|";
+            // extract value from instruction
+            int value_pos = instr.find_first_of(delim,0);
+            string value = instr.substr(0,value_pos);
+            cout<<value<<"\n";
+        }
+        else if(command == "GET"){
+            // extract key from instruction
+            int key_pos = instr.find_first_of(delim,0);
+            long long int key = stoll(instr.substr(0,key_pos));
+            instr = instr.substr(key_pos+1,instr.length());
+            cout<<key<<"|\n";
+        }
+        else if(command == "SCAN"){
+            // extract key1 from instruction
+            int key_pos = instr.find_first_of(delim,0);
+            long long int key_1 = stoll(instr.substr(0,key_pos));
+            instr = instr.substr(key_pos+1,instr.length());
+            cout<<key_1<<"|";
+            // extract key2 from instruction
+            key_pos = instr.find_first_of(delim,0);
+            long long int key_2 = stoll(instr.substr(0,key_pos));
+            instr = instr.substr(key_pos+1,instr.length());
+            cout<<key_2<<"|\n";
+        }
+
+    }
+
+    inputFile.close();
+    outputFile.close();
+    /* End here */
     double end_time = clock();
     cout<<"\nExecution time:"<< (end_time - start_time) / CLOCKS_PER_SEC << "(s)\n";
     return 0;
---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+## 編譯
+`g++ .\DataEngine.cpp -O3 -o DataEngine.exe `
+
+## 執行
+`./DataEngine.exe /*.input`
