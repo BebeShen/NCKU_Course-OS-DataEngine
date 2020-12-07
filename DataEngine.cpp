978a2cbd041ada427bdef6afa78d3f263eac46c6
BebeShen <55395848+BebeShen@users.noreply.github.com>
2020-12-07 14:59:19 +0800

    [feature] Implement `GET`,`PUT`,`SCAN`
    - Use data structure `map` to implement the key-value form with (`long long int`,`string`).
    - Use Buffer to store temporary data.
    - Add Description to README.
    
    TODO:
        [1] Write temporary buffer data into storage
        [2] Check whether `GET`,`SCAN` data is in storage.

--- DataEngine.cpp
+++ DataEngine.cpp
@@ -2,7 +2,14 @@
 #include<string.h>  
 #include<fstream>
 #include<sstream>  
-#include <ctime>
+#include<vector>
+
+/*map, unordered_map*/
+#include<queue>
+#include<map>
+#include<ctime>
+// set maxbuf size to (128 bytes*1000,000,0) = 1.28GB
+#define maxbuf 10000000
 using namespace std;
 string IntToString(int num){
     stringstream ss;
@@ -17,10 +24,11 @@ int main(int argc,char *argv[]){
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
     double start_time = clock();
-    string tmpfile_path = "/storage";
+    string tmpfile_path = "/storage/";
     int number_file = 0;
     string outputfile_path = "1.output";
     string inputfile_path = "";
+    map<long long int,string> buffer; 
     inputfile_path.append(argv[1]);
     // outputfile_path.append(outputfile_path);
     fstream inputFile(inputfile_path,ios_base::in);
@@ -34,6 +42,7 @@ int main(int argc,char *argv[]){
     }
     string instr;
     while(!inputFile.eof()){
+        //cout<<buffer.size()<<"\n";
         getline(inputFile,instr);
         const char* delim = " ";
         // extract command from instruction
@@ -51,6 +60,15 @@ int main(int argc,char *argv[]){
             int value_pos = instr.find_first_of(delim,0);
             string value = instr.substr(0,value_pos);
             cout<<value<<"\n";
+
+            /* Implement PUT instr */
+            if(buffer.size() <= maxbuf){
+                // buffer.insert(pair<long long int, string>(key,value));
+                buffer.insert({key,value});
+            }
+            else{
+                cout<<"Exceed the buffer limit!\n";
+            }
         }
         else if(command == "GET"){
             // extract key from instruction
@@ -58,6 +76,15 @@ int main(int argc,char *argv[]){
             long long int key = stoll(instr.substr(0,key_pos));
             instr = instr.substr(key_pos+1,instr.length());
             cout<<key<<"|\n";
+
+            /* Implement GET instr */
+            if(buffer.count(key)==0){
+                // TODO check storage
+                cout<<"EMPTY\n";
+            }
+            else{
+                cout<<buffer.at(key)<<"\n";
+            }
         }
         else if(command == "SCAN"){
             // extract key1 from instruction
@@ -70,10 +97,22 @@ int main(int argc,char *argv[]){
             long long int key_2 = stoll(instr.substr(0,key_pos));
             instr = instr.substr(key_pos+1,instr.length());
             cout<<key_2<<"|\n";
+
+            /* Implement SCAN instr */
+            for(int i=key_1;i<=key_2;++i){
+                if(buffer.count(i)!=0){
+                    cout<<buffer[i]<<"\n";
+                }else{
+                    // TODO check storage
+                }
+            }
         }
 
+        // if buffer is full, move data to storage
+        if(buffer.size() == maxbuf){
+            // TODO
+        }
     }
-
     inputFile.close();
     outputFile.close();
     /* End here */
