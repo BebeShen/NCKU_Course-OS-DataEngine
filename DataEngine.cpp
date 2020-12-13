70441591f0c31269e00eda46cd430325cf14ced4
BebeShen <55395848+BebeShen@users.noreply.github.com>
2020-12-14 01:22:12 +0800

    [Feature] DataEngine version 2.0 complete
    TODO:
        - Test Big data input
        - Evaluate the efficient
        - Optimize algorithm

--- DataEngine.cpp
+++ DataEngine.cpp
@@ -8,7 +8,7 @@
 #include<map>
 #include<ctime>
 // set maxbuf size to (128 bytes*1,000,000) = 128MB
-#define maxbuf 10
+#define maxbuf 1000000
 // #define PageSize INT32_MAX
 #define PageSize 4000
 using namespace std;
@@ -21,7 +21,6 @@ string IntToString(unsigned int num){
 }
 string CheckStorage(unsigned long long file_number,unsigned long long key){
     string file_path = "storage/";
-    cout<<file_number<<", "<<key<<"\n";
     fstream tmpfile(file_path+to_string(file_number),ios_base::in);
     if(!tmpfile){
         return "EMPTY";
@@ -48,10 +47,8 @@ string CheckStorage(unsigned long long file_number,unsigned long long key){
         }
         else{
             // find the key in storage
-            // cout<<"key:"<<_key<<"|";
             value_pos = tmp.find_first_of(delim,0);
             value = tmp.substr(0,value_pos);
-            // cout<<"value:"<<value<<"\n";
             return value;
         }
     }
@@ -59,27 +56,24 @@ string CheckStorage(unsigned long long file_number,unsigned long long key){
 }
 void DataToStorage(string file_path,map<unsigned long long,string>data){
     map<unsigned long long,string>stored_data;
-    cout<<"storage:"<<file_path<<"\n";
     fstream tmpfile(file_path,ios_base::in);
     map<unsigned long long, string>::iterator itr;
     if(!tmpfile){
         // storage doesn't exist
-        cout<<"Storage doesn't exist. Create one.\n";
         tmpfile.close();
         for(itr = data.begin();itr != data.end();++itr){
-            cout<<itr->first<<", "<<itr->second<<"\n";
             stored_data.insert({itr->first,itr->second});
         }
     }
     else{
         // if storage exist
-        cout<<"Storage Exist!\n";
         int key_pos,value_pos;
         unsigned long long _key;
         const char* delim = " ";
         string s,value;
         while(!tmpfile.eof()){
             getline(tmpfile,s);
+            if(s == "")continue;
             key_pos = s.find_first_of(delim,0);
             _key = atoll(s.substr(0,key_pos).c_str());
             s = s.substr(key_pos+1,s.length());
@@ -121,7 +115,6 @@ int main(int argc,char *argv[]){
     // outputfile_path.append(outputfile_path);
     fstream inputFile(inputfile_path,ios_base::in);
     fstream outputFile(outputfile_path,ios_base::out);
-    // cout<<"inputFile: "<<inputfile_path<<"\noutputFile: "<<outputfile_path<<endl;
     /* Code start here */
     // read command from inputfile
     if(!inputFile){
@@ -139,14 +132,11 @@ int main(int argc,char *argv[]){
         command_pos = instr.find_first_of(delim,0);
         command = instr.substr(0,command_pos);
         instr = instr.substr(command_pos+1,instr.length());
-        cout<<command<<"|";
         if(command == "PUT"){
             // extract key from instruction
             key_pos = instr.find_first_of(delim,0);
-            // cout<<key_pos<<endl;
             unsigned long long key = atoll(instr.substr(0,key_pos).c_str());
             instr = instr.substr(key_pos+1,instr.length());
-            // cout<<key<<"|";
             // extract value from instruction
             value_pos = instr.find_first_of(delim,0);
             value = instr.substr(0,value_pos);
@@ -154,7 +144,6 @@ int main(int argc,char *argv[]){
 
             /* Implement PUT instr */
             if(buffer.size() <= maxbuf){
-                // buffer.insert(pair<unsigned long long int, string>(key,value));
                 if(buffer.count(key)==0){
                     // buffer doesn't have the data
                     buffer.insert({key,value});
@@ -172,16 +161,13 @@ int main(int argc,char *argv[]){
             key_pos = instr.find_first_of(delim,0);
             unsigned long long key = atoll(instr.substr(0,key_pos).c_str());
             instr = instr.substr(key_pos+1,instr.length());
-            //cout<<key<<"|\n";
 
             /* Implement GET instr */
             if(buffer.count(key)==0){
                 string tmp = CheckStorage(key/PageSize,key%PageSize);
-                // cout<<CheckStorage(key/INT32_MAX,key%INT32_MAX)<<"\n";
                 outputFile<<tmp<<"\n";
             }
             else{
-                cout<<buffer.at(key)<<"\n";
                 outputFile<<buffer.at(key)<<"\n";
             }
         }
@@ -190,21 +176,17 @@ int main(int argc,char *argv[]){
             key_pos = instr.find_first_of(delim,0);
             unsigned long long key_1 = atoll(instr.substr(0,key_pos).c_str());
             instr = instr.substr(key_pos+1,instr.length());
-            //cout<<key_1<<"|";
             // extract key2 from instruction
             key_pos = instr.find_first_of(delim,0);
             unsigned long long key_2 = atoll(instr.substr(0,key_pos).c_str());
             instr = instr.substr(key_pos+1,instr.length());
-            //cout<<key_2<<"|\n";
 
             /* Implement SCAN instr */
             for(unsigned long long i=key_1;i<=key_2;++i){
                 if(buffer.count(i)>0){
-                    //cout<<buffer[i]<<"\n";
                     outputFile<<buffer.at(i)<<"\n";
                 }else{
                     string tmp = CheckStorage(i/PageSize,i%PageSize);
-                    //cout<<CheckStorage(i/INT32_MAX,i%INT32_MAX)<<"\n";
                     outputFile<<tmp<<"\n";
                 }
             }
@@ -218,10 +200,6 @@ int main(int argc,char *argv[]){
             map<unsigned long long, string>::iterator itr;
             unsigned long long store_number;
             for (itr = buffer.begin();itr != buffer.end();++itr) { 
-                cout << itr->first <<", " << itr->second <<"\n"; 
-            }cout<<"\n\n";
-            for (itr = buffer.begin();itr != buffer.end();++itr) { 
-                cout << itr->first <<", " << itr->second <<"\n"; 
                 // page data into storage classified by key's upper 32-bit
                 store_number = itr->first/PageSize;
                 cache.insert({itr->first%PageSize,itr->second});
@@ -238,7 +216,7 @@ int main(int argc,char *argv[]){
         //     cout << itr->first <<", " << itr->second <<"\n"; 
         // } 
     }
-    cout<<"Buffer:"<<buffer.size()<<"\n";
+    // cout<<"Buffer:"<<buffer.size()<<"\n";
     inputFile.close();
     outputFile.close();
     /* End here */
--- generateInput.py
+++ generateInput.py
@@ -13,34 +13,36 @@ random.seed(time.time())
 # Set ENV
 maxValue = pow(2,31)-1 # 2^31-1
 minValue = 0
-number_input = 1000
+number_input = 5000
 # file_path = "small_" + str(number_input)+'.input'
-file_path = "all_PUT_1000.input"
+file_path = "5000.input"
 method = ['PUT','GET','SCAN']
 seed = "0123456789qazxswedcvfrtgbnhyujmkiolp"
 
 with open(file_path,'w') as f:
     instrs = []
     for i in range(number_input):
-        which = random.randint(1,7)
+        which = random.randint(1,9)
         if which <= 7:
             # PUT
             s = []
             for _ in range(128):
                 s.append(random.choice(seed))
             # instr = "PUT" + " " + str(random.randint(minValue,maxValue)) + " " + ''.join(s)
-            instr = "PUT" + " " + str(random.randint(0,4000*2)) + " " + ''.join(s)
+            instr = "PUT" + " " + str(random.randint(0,7999)) + " " + ''.join(s)
             # print(instr)
             instrs.append(instr+"\n")
         elif which <= 9:
             # GET
-            instr = "GET" + " " + str(random.randint(minValue,maxValue))
+            # instr = "GET" + " " + str(random.randint(minValue,maxValue))
+            instr = "GET" + " " + str(random.randint(0,7999))
             # print(instr)
             instrs.append(instr+"\n")
         elif which == 10:
             # SCAN
-            k1 = random.randint(minValue,(int)((maxValue / 4) * 3) )
-            k2 = k1 + random.randint(250,5000)
+            # k1 = random.randint(minValue,(int)((maxValue / 4) * 3) )
+            k1 = random.randint(0,6999)
+            k2 = k1 + random.randint(100,1000)
             instr = "SCAN" + " " + str(k1) + " " + str(k2)
             # print(instr)
             instrs.append(instr+"\n")
